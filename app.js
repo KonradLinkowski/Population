dba5bbdcd91d57721f3eb73e88d99ed0973e4510
Konrad Linkowski <linkowski.konrad97@gmail.com>
2017-09-04 17:16:05 +0200

    Organized code with classes, changed colours to string
    Now colours are represented as hexadecimal string ("#ff0000") instead of
    number.
    Added classes Game and Util.
    Now there is possibility to calculate average vitality of each colony.

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
+//getColour dziala na koloniach, a kolory na enumie, bo przesz jest jeszcze grass i water
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-var Colours;
-(function (Colours) {
-    Colours[Colours["GRASS"] = 65280] = "GRASS";
-    Colours[Colours["WATER"] = 255] = "WATER";
-    Colours[Colours["RED"] = 16711680] = "RED";
-    Colours[Colours["PURPLE"] = 11796735] = "PURPLE";
-    Colours[Colours["YELLOW"] = 16776960] = "YELLOW";
-    Colours[Colours["MAGENTA"] = 16711935] = "MAGENTA";
-})(Colours || (Colours = {}));
-function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
+var Util = (function () {
+    function Util() {
+    }
+    Util.randomInt = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    Util.toColour = function (num) {
+        var s = "000000" + num.toString(16);
+        return "#" + s.substr(s.length - 6);
+    };
+    return Util;
+}());
 var Map = (function () {
-    function Map(canvas, coloniesNumber) {
-        if (coloniesNumber === void 0) { coloniesNumber = 3; }
+    function Map(game, canvas, coloniesNumber) {
         this.coloniesNumber = coloniesNumber;
+        this.game = game;
         this.canvas = canvas;
         this._height = canvas.height;
         this._width = canvas.width;
@@ -31,16 +32,16 @@ var Map = (function () {
             this.map[i] = [];
         }
         for (var i = 0; i < imageData.length; i += 4) {
-            color = (imageData[i] << 16) | (imageData[i + 1] << 8) | (imageData[i + 2]);
-            if (color == Colours.GRASS) {
+            var color_1 = Util.toColour((imageData[i] << 16) | (imageData[i + 1] << 8) | (imageData[i + 2]));
+            if (color_1 == this.game.getColour(this.game.GRASSCOLOUR)) {
                 x = (i / 4) % this._width;
                 y = Math.floor((i / 4) / this._width);
-                this.map[x][y] = GRASSTILE;
+                this.map[x][y] = this.game.GRASSTILE;
             }
-            if (color == Colours.WATER) {
+            if (color_1 == this.game.getColour(this.game.WATERCOLOUR)) {
                 x = (i / 4) % this._width;
                 y = Math.floor((i / 4) / this._width);
-                this.map[x][y] = WATERTILE;
+                this.map[x][y] = this.game.WATERTILE;
             }
         }
         this.createColonies();
@@ -64,9 +65,7 @@ var Map = (function () {
     };
     Map.prototype.setObj = function (x, y, obj) {
         this.map[x][y] = obj;
-        var temp = obj.colour.toString(16);
-        temp = pad(temp, 6);
-        this.context.fillStyle = "#" + temp;
+        this.context.fillStyle = (this.game.getColour(obj.colour));
         this.context.fillRect(x, y, 1, 1);
     };
     Map.prototype.killPerson = function (x, y, killer) {
@@ -77,19 +76,19 @@ var Map = (function () {
         var x, y, offset = 10, x2, y2;
         for (var i = 0; i < this.coloniesNumber; i++) {
             while (true) {
-                x = randomInt(0, this._width);
-                y = randomInt(0, this._height);
-                if (this.map[x][y].colour == Colours.GRASS) {
+                x = Util.randomInt(0, this._width);
+                y = Util.randomInt(0, this._height);
+                if (this.map[x][y].colour == this.game.GRASSCOLOUR) {
                     break;
                 }
             }
             for (var j = 0; j < 50; j++) {
                 while (true) {
-                    x2 = x + randomInt(0, offset);
-                    y2 = y + randomInt(0, offset);
+                    x2 = x + Util.randomInt(0, offset);
+                    y2 = y + Util.randomInt(0, offset);
                     //window.alert("i " + i + " j " + j + " x " + x + " y " + y + " x2 " + x2 + " y2 " + y2);
-                    if (this.map[x2][y2].colour == Colours.GRASS) {
-                        this.setObj(x2, y2, new Person(this, x2, y2, 0, randomInt(0, 100), coloniesColours[i], randomInt(0, 100)));
+                    if (this.map[x2][y2].colour == this.game.GRASSCOLOUR) {
+                        this.setObj(x2, y2, new Person(game, this, x2, y2, 0, Util.randomInt(0, 100), i, Util.randomInt(0, 100)));
                         break;
                     }
                 }
@@ -116,13 +115,14 @@ var Tile = (function () {
 }());
 var Person = (function (_super) {
     __extends(Person, _super);
-    function Person(map, x, y, age, reproductionValue, colour, vitality) {
-        if (age === void 0) { age = randomInt(20, 70); }
-        if (reproductionValue === void 0) { reproductionValue = randomInt(20, 70); }
-        if (colour === void 0) { colour = coloniesColours[randomInt(0, coloniesColours.length)]; }
-        if (vitality === void 0) { vitality = randomInt(20, 70); }
+    function Person(game, map, x, y, age, reproductionValue, colour, vitality) {
+        if (age === void 0) { age = Util.randomInt(20, 70); }
+        if (reproductionValue === void 0) { reproductionValue = 0; }
+        if (colour === void 0) { colour = 0; }
+        if (vitality === void 0) { vitality = Util.randomInt(20, 70); }
         _super.call(this, colour);
-        this.dead = true;
+        this.dead = false;
+        this.game = game;
         this.x = x;
         this.y = y;
         this.age = age;
@@ -130,36 +130,31 @@ var Person = (function (_super) {
         this.reproductionValue = 0;
         this.vitality = vitality;
         this.map = map;
-        Person.list.push(this);
+        this.game.colonyPush(this);
     }
-    Person.prototype.mortality = function () {
-        return 1;
-        /*
-        if (this.age < 30) {
-            return (-1 / 2) * this.age + 30;
-        } else {
-            return this.age;
-        }
-        */
-    };
     Person.prototype.shouldDie = function () {
-        return this.vitality < this.age * 4;
+        return this.vitality < this.age;
         //return randomInt(0, 100) <= this.mortality();
     };
     Person.prototype.move = function () {
         this.age++;
         this.reproductionValue++;
-        var x = Math.random() < 0.5 ? -1 : 1;
-        var y = Math.random() < 0.5 ? -1 : 1;
+        //let x = Math.random() < 0.5 ? -1 : 1;
+        //let y = Math.random() < 0.5 ? -1 : 1;
+        var x, y;
+        do {
+            x = Util.randomInt(-1, 1);
+            y = Util.randomInt(-1, 1);
+        } while ((x == 0 && y == 0));
         switch (this.map.getObj(this.x + x, this.y + y).colour) {
-            case Colours.GRASS:
-                this.map.setObj(this.x, this.y, GRASSTILE);
+            case this.game.GRASSCOLOUR:
+                this.map.setObj(this.x, this.y, this.game.GRASSTILE);
                 this.reproduce();
                 this.x += x;
                 this.y += y;
                 this.map.setObj(this.x, this.y, this);
                 break;
-            case Colours.WATER:
+            case this.game.WATERCOLOUR:
                 //this.vitality *= 1.2;
                 break;
             case this.colour:
@@ -167,73 +162,107 @@ var Person = (function (_super) {
                 break;
             default:
                 if (this.vitality >= this.map.getObj(this.x + x, this.y + y).vitality) {
-                    this.map.setObj(this.x, this.y, GRASSTILE);
+                    this.map.setObj(this.x, this.y, this.game.GRASSTILE);
                     this.reproduce();
                     this.x += x;
                     this.y += y;
                     this.map.killPerson(this.x, this.y, this);
                 }
                 else {
-                    this.map.setObj(this.x, this.y, GRASSTILE);
+                    this.map.setObj(this.x, this.y, this.game.GRASSTILE);
                     this.kill();
                 }
                 break;
         }
         if (this.shouldDie()) {
-            this.map.setObj(this.x, this.y, GRASSTILE);
+            this.map.setObj(this.x, this.y, this.game.GRASSTILE);
             this.kill();
         }
     };
     Person.prototype.reproduce = function () {
-        if (this.reproductionValue > randomInt(0, this.reproductionValue)) {
+        if (this.reproductionValue < this.game.reproductiveThreshold) {
             return;
         }
-        this.map.setObj(this.x, this.y, new Person(this.map, this.x, this.y, 0, randomInt(0, 100), this.colour, this.vitality));
+        this.reproductionValue = 0;
+        this.map.setObj(this.x, this.y, new Person(this.game, this.map, this.x, this.y, 0, Util.randomInt(0, 100), this.colour, this.vitality));
     };
     Person.prototype.kill = function () {
         this.dead = true;
-        var index = Person.list.indexOf(this, 0);
-        if (index > -1) {
-            Person.list.splice(index, 1);
-        }
+        this.game.colonyRemove(this);
     };
-    Person.list = [];
     return Person;
 }(Tile));
-function pad(num, size) {
-    var s = "00000000" + num;
-    return s.substr(s.length - size);
-}
-function game() {
-    for (var i = 0; i < Person.list.length; i++) {
-        Person.list[i].move();
+var Game = (function () {
+    function Game(canvas, coloniesNumber) {
+        var _this = this;
+        this.ageCount = 0;
+        this.colours = ["#ffff00", "#ff00ff", "#ff0000", "#bb00ff", "#00ff00", "#0000ff"];
+        //coloniesColours = [Colours.YELLOW, Colours.MAGENTA, Colours.RED, Colours.PURPLE];
+        this.coloniesColours = this.colours.length - 2;
+        this.colonies = [];
+        this.GRASSCOLOUR = this.colours.length - 2;
+        this.WATERCOLOUR = this.colours.length - 1;
+        this.GRASSTILE = new Tile(this.GRASSCOLOUR);
+        this.WATERTILE = new Tile(this.WATERCOLOUR);
+        this.reproductiveThreshold = 20;
+        this.image = new Image();
+        this.peopleCount = 0;
+        this.canvas = canvas;
+        this.coloniesNumber = coloniesNumber;
+        this.image.onload = function () {
+            _this.setup();
+        };
+        this.image.src = "mapa4.png";
     }
-    testSpan.innerHTML = 'People: ' + Person.list.length;
-    age.innerHTML = "Age: " + ageCount++;
-}
-var ageCount = 0;
-var testSpan;
+    Game.prototype.setup = function () {
+        this.canvas.width = this.image.width;
+        this.canvas.height = this.image.height;
+        this.canvas.getContext('2d').drawImage(this.image, 0, 0);
+        for (var i = 0; i < this.coloniesColours; i++) {
+            this.colonies[i] = [];
+        }
+        this.map = new Map(this, this.canvas, this.coloniesNumber);
+        setInterval(this.play.bind(this), 200);
+        for (var i = 0; i < this.coloniesNumber; i++) {
+            test.innerHTML += '<span></span><br/>';
+        }
+        this.coloniesLabels = test.getElementsByTagName("span");
+    };
+    Game.prototype.play = function () {
+        for (var i = 0; i < this.colonies.length; i++) {
+            for (var j = 0; j < this.colonies[i].length; j++) {
+                this.colonies[i][j].move();
+                this.coloniesLabels[i].innerHTML = "Colony " + (i + 1) + ": " + this.colonies[i].length;
+            }
+        }
+        age.innerHTML = "Age: " + this.ageCount++;
+    };
+    Game.prototype.getColour = function (index) {
+        return this.colours[index];
+    };
+    Game.prototype.colonyPush = function (obj) {
+        this.colonies[obj.colour].push(obj);
+        this.peopleCount++;
+    };
+    Game.prototype.colonyRemove = function (obj) {
+        var index = this.colonies[obj.colour].indexOf(obj, 0);
+        if (index > -1) {
+            this.colonies[obj.colour].splice(index, 1);
+        }
+        this.peopleCount--;
+    };
+    return Game;
+}());
 var age;
 var test;
-var coloniesColours = [Colours.YELLOW, Colours.MAGENTA, Colours.RED, Colours.PURPLE];
-var colonies = [];
-var GRASSTILE = new Tile(Colours.GRASS);
-var WATERTILE = new Tile(Colours.WATER);
-var image = new Image();
-image.src = "mapa4.png";
+var game;
 window.onload = function () {
     test = document.getElementById('test');
     var canvas = document.getElementById('canvas');
-    canvas.width = image.width;
-    canvas.height = image.height;
-    canvas.getContext('2d').drawImage(image, 0, 0);
     canvas.getContext('2d').webkitImageSmoothingEnabled = false;
     canvas.getContext('2d').mozImageSmoothingEnabled = false;
     canvas.getContext('2d').imageSmoothingEnabled = false; /// future
-    var map = new Map(canvas);
-    test.innerHTML += '<span id="testSpan">People: ' + Person.list.length + '</span>';
-    testSpan = document.getElementById('testSpan');
+    game = new Game(canvas, 4);
     age = document.getElementById('age');
-    setInterval(game, 200);
 };
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,14 +1,17 @@
-﻿enum Colours {
-    GRASS = 255 << 8,
-    WATER = 255,
-    RED = 255 << 16,
-    PURPLE = (180 << 16) | 255,
-    YELLOW = (255 << 16) | (255 << 8),
-    MAGENTA = (255 << 16) | 255
-}
+﻿//getColour dziala na koloniach, a kolory na enumie, bo przesz jest jeszcze grass i water
 
-function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+class Util {
+
+    static randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    
+    static toColour(num: number) {
+        let s: string = "000000" + num.toString(16);
+        return "#" + s.substr(s.length - 6);
+    }
+    
 }
 
 class Map {
@@ -18,15 +21,17 @@ class Map {
     private _height: number;
     private _width: number;
     private coloniesNumber;
+    private game: Game;
 
-    constructor(canvas: HTMLCanvasElement, coloniesNumber: number = 3) {
+    constructor(game: Game, canvas: HTMLCanvasElement, coloniesNumber: number) {
         this.coloniesNumber = coloniesNumber;
+        this.game = game;
         this.canvas = canvas;
         this._height = canvas.height;
         this._width = canvas.width;
         this.context = canvas.getContext('2d');
         let imageData = this.context.getImageData(0, 0, this._width, this._height).data;
-        
+
         //#1a315c
         let x, y, color;
         this.map = [];
@@ -34,16 +39,16 @@ class Map {
             this.map[i] = [];
         }
         for (let i = 0; i < imageData.length; i += 4) {
-            color = (imageData[i] << 16) | (imageData[i + 1] << 8) | (imageData[i + 2]);
-            if (color == Colours.GRASS) {
+            let color: string = Util.toColour((imageData[i] << 16) | (imageData[i + 1] << 8) | (imageData[i + 2]));
+            if (color == this.game.getColour(this.game.GRASSCOLOUR)) {
                 x = (i / 4) % this._width;
                 y = Math.floor((i / 4) / this._width);
-                this.map[x][y] = GRASSTILE;
+                this.map[x][y] = this.game.GRASSTILE;
             }
-            if (color == Colours.WATER) {
+            if (color == this.game.getColour(this.game.WATERCOLOUR)) {
                 x = (i / 4) % this._width;
                 y = Math.floor((i / 4) / this._width);
-                this.map[x][y] = WATERTILE;
+                this.map[x][y] = this.game.WATERTILE;
             }
         }
         this.createColonies();
@@ -63,9 +68,7 @@ class Map {
 
     setObj(x: number, y: number, obj: Tile): void {
         this.map[x][y] = obj;
-        let temp: string = obj.colour.toString(16);
-        temp = pad(temp, 6);
-        this.context.fillStyle = "#" + temp;
+        this.context.fillStyle = (this.game.getColour(obj.colour));
         this.context.fillRect(x, y, 1, 1);
     }
 
@@ -78,19 +81,19 @@ class Map {
         let x, y, offset = 10, x2, y2;
         for (let i: number = 0; i < this.coloniesNumber; i++) {
             while (true) {
-                x = randomInt(0, this._width);
-                y = randomInt(0, this._height);
-                if (this.map[x][y].colour == Colours.GRASS) {
+                x = Util.randomInt(0, this._width);
+                y = Util.randomInt(0, this._height);
+                if (this.map[x][y].colour == this.game.GRASSCOLOUR) {
                     break;
                 }
             } 
             for (let j: number = 0; j < 50; j++) {
                 while (true) {
-                    x2 = x + randomInt(0, offset);
-                    y2 = y + randomInt(0, offset);
+                    x2 = x + Util.randomInt(0, offset);
+                    y2 = y + Util.randomInt(0, offset);
                     //window.alert("i " + i + " j " + j + " x " + x + " y " + y + " x2 " + x2 + " y2 " + y2);
-                    if (this.map[x2][y2].colour == Colours.GRASS) {
-                        this.setObj(x2, y2, new Person (this, x2, y2, 0, randomInt(0, 100), coloniesColours[i], randomInt(0, 100)));
+                    if (this.map[x2][y2].colour == this.game.GRASSCOLOUR) {
+                        this.setObj(x2, y2, new Person (game, this, x2, y2, 0, Util.randomInt(0, 100), i, Util.randomInt(0, 100)));
                         break;
                     }
                 }
@@ -117,18 +120,19 @@ class Tile {
 }
 
 class Person extends Tile {
-    public static list: Array<Person> = [];
     private x: number;
     private y: number;
-    private dead: boolean = true;
+    private dead: boolean = false;
     private age: number;
     private reproductionValue: number;
     private vitality: number;
     private map: Map;
+    private game: Game;
 
-    constructor(map: Map, x: number, y: number, age: number = randomInt(20, 70), reproductionValue: number = randomInt(20, 70),
-        colour: number = coloniesColours[randomInt(0, coloniesColours.length)], vitality: number = randomInt(20, 70)) {
+    constructor(game: Game, map: Map, x: number, y: number, age: number = Util.randomInt(20, 70), reproductionValue: number = 0,
+        colour: number = 0, vitality: number = Util.randomInt(20, 70)) {
         super(colour);
+        this.game = game;
         this.x = x;
         this.y = y;
         this.age = age;
@@ -136,39 +140,33 @@ class Person extends Tile {
         this.reproductionValue = 0;
         this.vitality = vitality;
         this.map = map;
-        Person.list.push(this);
-    }
-
-    mortality(): number {
-        return 1;
-        /*
-        if (this.age < 30) {
-            return (-1 / 2) * this.age + 30;
-        } else {
-            return this.age;
-        }
-        */
+        this.game.colonyPush(this);
     }
 
     shouldDie(): boolean {
-        return this.vitality < this.age * 4;
+        return this.vitality < this.age;
         //return randomInt(0, 100) <= this.mortality();
     }
 
     public move(): void {
         this.age++;
         this.reproductionValue++;
-        let x = Math.random() < 0.5 ? -1 : 1;
-        let y = Math.random() < 0.5 ? -1 : 1;
+        //let x = Math.random() < 0.5 ? -1 : 1;
+        //let y = Math.random() < 0.5 ? -1 : 1;
+        let x, y;
+        do {
+            x = Util.randomInt(-1, 1);
+            y = Util.randomInt(-1, 1);
+        } while ((x == 0 && y == 0));
         switch (this.map.getObj(this.x + x, this.y + y).colour) {
-            case Colours.GRASS:
-                this.map.setObj(this.x, this.y, GRASSTILE);
+            case this.game.GRASSCOLOUR:
+                this.map.setObj(this.x, this.y, this.game.GRASSTILE);
                 this.reproduce();
                 this.x += x;
                 this.y += y;
                 this.map.setObj(this.x, this.y, this);
                 break;
-            case Colours.WATER:
+            case this.game.WATERCOLOUR:
                 //this.vitality *= 1.2;
                 break;
             case this.colour:
@@ -176,76 +174,129 @@ class Person extends Tile {
                 break;
             default:
                 if (this.vitality >= (<Person>this.map.getObj(this.x + x, this.y + y)).vitality) {
-                    this.map.setObj(this.x, this.y, GRASSTILE);
+                    this.map.setObj(this.x, this.y, this.game.GRASSTILE);
                     this.reproduce();
                     this.x += x;
                     this.y += y;
                     this.map.killPerson(this.x, this.y, this);
                 } else {
-                    this.map.setObj(this.x, this.y, GRASSTILE);
+                    this.map.setObj(this.x, this.y, this.game.GRASSTILE);
                     this.kill();
                 }
                 break;
         }
         if (this.shouldDie()) {
-            this.map.setObj(this.x, this.y, GRASSTILE);
+            this.map.setObj(this.x, this.y, this.game.GRASSTILE);
             this.kill();
         }
     }
 
     reproduce(): void {
-        if (this.reproductionValue > randomInt(0, this.reproductionValue)) {
+        if (this.reproductionValue < this.game.reproductiveThreshold) {
             return;
         }
-        this.map.setObj(this.x, this.y, new Person(this.map, this.x, this.y, 0, randomInt(0, 100), this.colour, this.vitality));
+        this.reproductionValue = 0;
+        this.map.setObj(this.x, this.y, new Person(this.game, this.map, this.x, this.y, 0, Util.randomInt(0, 100), this.colour, this.vitality));
     }
 
     kill(): void {
         this.dead = true;
-        var index = Person.list.indexOf(this, 0);
-        if (index > -1) {
-            Person.list.splice(index, 1);
+        this.game.colonyRemove(this);
+    }
+}
+
+class Game {
+
+    ageCount: number = 0;
+    colours: string[] = ["#ffff00", "#ff00ff", "#ff0000", "#bb00ff", "#00ff00", "#0000ff"];
+    //coloniesColours = [Colours.YELLOW, Colours.MAGENTA, Colours.RED, Colours.PURPLE];
+    coloniesColours: number = this.colours.length - 2;
+    colonies: Array<Person[]> = [];
+
+    coloniesNumber: number;
+
+    GRASSCOLOUR = this.colours.length - 2;
+    WATERCOLOUR = this.colours.length - 1;
+
+    GRASSTILE = new Tile(this.GRASSCOLOUR);
+    WATERTILE = new Tile(this.WATERCOLOUR);
+    reproductiveThreshold = 20;
+
+    image = new Image();
+    map: Map;
+    canvas: HTMLCanvasElement;
+
+    coloniesLabels;
+
+    peopleCount = 0;
+
+    constructor(canvas: HTMLCanvasElement, coloniesNumber: number) {
+        this.canvas = canvas;
+        this.coloniesNumber = coloniesNumber;
+        this.image.onload = () => {
+            this.setup();
+        };
+        this.image.src = "mapa4.png";
+    }
+
+    setup(): void  {
+        this.canvas.width = this.image.width;
+        this.canvas.height = this.image.height;
+        this.canvas.getContext('2d').drawImage(this.image, 0, 0);
+        for (let i: number = 0; i < this.coloniesColours; i++) {
+            this.colonies[i] = [];
         }
+        this.map = new Map(this, <HTMLCanvasElement>this.canvas, this.coloniesNumber)
+        setInterval(this.play.bind(this), 200);
+        for (let i: number = 0; i < this.coloniesNumber; i++) {
+            test.innerHTML += '<span></span><br/>';
+        }
+        this.coloniesLabels = test.getElementsByTagName("span");
     }
-}
 
-function pad(num, size) {
-    var s = "00000000" + num;
-    return s.substr(s.length - size);
-}
-
-function game(): void {
-    for (let i: number = 0; i < Person.list.length; i++) {
-        Person.list[i].move();
+    play(): void {
+        for (let i: number = 0; i < this.colonies.length; i++) {
+            for (let j: number = 0; j < this.colonies[i].length; j++) {
+                this.colonies[i][j].move();
+                this.coloniesLabels[i].innerHTML = "Colony " + (i + 1) + ": " + this.colonies[i].length;
+            }
+        }
+        age.innerHTML = "Age: " + this.ageCount++;
     }
-    testSpan.innerHTML = 'People: ' + Person.list.length;
-    age.innerHTML = "Age: " + ageCount++;
+
+    getColour(index: number): string {
+        return this.colours[index];
+    }
+
+    colonyPush(obj: Person): void {
+        this.colonies[obj.colour].push(obj);
+        this.peopleCount++;
+    }
+
+    colonyRemove(obj: Person): void {
+        let index = this.colonies[obj.colour].indexOf(obj, 0);
+        if (index > -1) {
+            this.colonies[obj.colour].splice(index, 1);
+        }
+        this.peopleCount--;
+    }
+
 }
 
-let ageCount: number = 0;
-let testSpan: HTMLElement;
+
 let age: HTMLElement;
 let test: HTMLElement
-let coloniesColours = [Colours.YELLOW, Colours.MAGENTA, Colours.RED, Colours.PURPLE];
-let colonies: Array<Person> = [];
-const GRASSTILE = new Tile(Colours.GRASS);
-const WATERTILE = new Tile(Colours.WATER);
+let game: Game;
 
-let image = new Image();
-image.src = "mapa4.png";
 window.onload = () => {
     test = document.getElementById('test');
     let canvas: any = document.getElementById('canvas');
-    canvas.width = image.width;
-    canvas.height = image.height;
-    canvas.getContext('2d').drawImage(image, 0, 0);
+    
     canvas.getContext('2d').webkitImageSmoothingEnabled = false;
     canvas.getContext('2d').mozImageSmoothingEnabled = false;
     canvas.getContext('2d').imageSmoothingEnabled = false; /// future
-    let map = new Map(<HTMLCanvasElement>canvas);
 
-    test.innerHTML += '<span id="testSpan">People: ' + Person.list.length + '</span>';
-    testSpan = document.getElementById('testSpan');
+    game = new Game(canvas, 4);
     age = document.getElementById('age');
-    setInterval(game, 200);
+
 };
