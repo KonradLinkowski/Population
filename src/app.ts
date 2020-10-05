9423fc6bc5c4971b869a8e4e7f1b58ba4f89c60d
Nicolaj Gajhede <ngajhede@gmail.com>
2020-10-05 18:38:17 +0200

    fix: upload button overflowing viewport on mobile

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,12 @@ import HTMLConnector from "./html_connector";
 window.onload = () => {
   const htCon = new HTMLConnector();
   htCon.startGame();
+  
+  const uploadBtn = document.getElementById('add_map');
+  
+  const fileName = document.getElementById('upload-label');
+  
+  uploadBtn.addEventListener('change', function(){
+    fileName.textContent = this.files[0].name;
+  })
 };
