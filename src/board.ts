785ebbf2de878d2766c5fea4bf060c7bbe5b0fff
Lucas Leonardo <lucasleonardo@protonmail.com>
2020-10-21 21:27:46 -0300

    change from hex to hsl

--- src/board.ts
+++ src/board.ts
@@ -3,6 +3,7 @@ import Person from "./person";
 import Tile from "./tile";
 import Util from "./util";
 import RGBColor from "./rgb_color";
+import { HSLColor } from "./hsl_color";
 
 export default class Board {
   private canvas: HTMLCanvasElement;
@@ -13,9 +14,6 @@ export default class Board {
   private width: number;
   private coloniesNumber: number;
   private game: Game;
-  private rgbCache: {
-    [key: string]: RGBColor
-  } = {};
 
   constructor(game: Game, canvas: HTMLCanvasElement, coloniesNumber: number) {
     this.coloniesNumber = coloniesNumber;
@@ -31,11 +29,11 @@ export default class Board {
       this.map[i] = [];
     }
     for (let i = 0; i < this.imageData.data.length; i += 4) {
-      const color: string =
-        "#" +
-        Util.int2hex(this.imageData.data[i]) +
-        Util.int2hex(this.imageData.data[i + 1]) +
-        Util.int2hex(this.imageData.data[i + 2]);
+      const r = this.imageData.data[i];
+      const g = this.imageData.data[i + 1];
+      const b = this.imageData.data[i + 2];
+      const color: HSLColor = Util.rgb2hsl(r, g, b);
+
       if (color === this.game.getColour(this.game.GRASS_COLOUR)) {
         const x = (i / 4) % this.width;
         const y = Math.floor(i / 4 / this.width);
@@ -77,15 +75,8 @@ export default class Board {
 
   private getRGBColour(index: number): RGBColor {
     const color = this.game.getColour(index);
-    if (!(color in this.rgbCache)) {
-      const match = /#([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])/i.exec(color);
-      this.rgbCache[color] = new RGBColor(
-        parseInt(match[1], 16),
-        parseInt(match[2], 16),
-        parseInt(match[3], 16)
-      );
-    }
-    return this.rgbCache[color];
+    const [r, g, b] = Util.hsl2rgb(color);
+    return new RGBColor(r, g, b);
   }
 
   public redraw(): void {
--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,9 @@
 import Person from "./person";
+import { HSLColor } from "./hsl_color";
+
+const ONE_SIXTH = 1 / 6;
+const ONE_THIRD = 1 / 3;
+const TWO_THIRDS = 2 / 3;
 
 export default class Util {
   // Random int between min inclusive and max exclusive.
@@ -54,5 +59,71 @@ export default class Util {
       }
     }
     return imageData;
+  }
+
+  private static hue2rgb(p: number, q: number, t: number): number {
+    if (t < 0) {
+      t += 1;
+    }
+    if (t > 1) {
+      t -= 1;
+    }
+    if (t < ONE_SIXTH) {
+      return p + (q - p) * 6 * t;
+    }
+    if (t < 0.5) {
+      return q;
+    }
+    if (t < TWO_THIRDS) {
+      return p + (q - p) * (TWO_THIRDS - t) * 6;
+    }
+    return p;
+  }
+
+  public static hsl2rgb(col: HSLColor): number[] {
+    const [h, s, l] = col;
+    if (s === 0) {
+      return [l, l, l];
+    }
+    const q = l < 0.5 ? l * s + l : l + s - l * s;
+    const p = 2 * l - q;
+    return [
+      this.hue2rgb(p, q, h + ONE_THIRD),
+      this.hue2rgb(p, q, h),
+      this.hue2rgb(p, q, h - ONE_THIRD),
+    ];
+  }
+
+  public static rgb2hsl(r: number, g: number, b: number): HSLColor {
+    (r /= 255), (g /= 255), (b /= 255);
+
+    const max = Math.max(r, g, b);
+    const min = Math.min(r, g, b);
+    let h = (max + min) / 2;
+    let s = (max + min) / 2;
+    const l = (max + min) / 2;
+
+    if (max === min) {
+      h = s = 0; // achromatic
+    } else {
+      const d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+      switch (max) {
+        case r:
+          h = (g - b) / d + (g < b ? 6 : 0);
+          break;
+        case g:
+          h = (b - r) / d + 2;
+          break;
+        case b:
+          h = (r - g) / d + 4;
+          break;
+      }
+
+      h /= 6;
+    }
+
+    return new HSLColor(h, s, l);
   }
 }
